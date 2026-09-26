965f9d219b67e01dab71134526f2588318cfc3a3
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2026-09-26 18:07:03 +0800

    Support direct business profile ratings

--- server/models/Rating.js
+++ server/models/Rating.js
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 
 const ratingSchema = new mongoose.Schema(
   {
-    content: { type: mongoose.Schema.Types.ObjectId, ref: "Content", required: true, index: true },
+    content: { type: mongoose.Schema.Types.ObjectId, ref: "Content", default: null, index: true },
+    business: { type: mongoose.Schema.Types.ObjectId, ref: "Business", default: null, index: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
     rating: { type: Number, required: true, min: 1, max: 5 }
   },
   { timestamps: true }
 );
 
-ratingSchema.index({ content: 1, user: 1 }, { unique: true });
+ratingSchema.index({ content: 1, user: 1 }, { unique: true, partialFilterExpression: { content: { $type: "objectId" } } });
+ratingSchema.index({ business: 1, user: 1 }, { unique: true, partialFilterExpression: { business: { $type: "objectId" } } });
 
 export default mongoose.model("Rating", ratingSchema);
